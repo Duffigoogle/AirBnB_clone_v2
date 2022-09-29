a089f7b13931e9c911cc2d02673f05ea9642f6ab
duffigoogle <calebdduff@gmail.com>
2022-09-29 22:35:20 +0000

    Add project file for Task 2- Deploy archive

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Deploy archive...Fabric script that generates a .tgz archive 
+from the contents of the web_static folder.
+Distributes an archive to a wb server"""
+
+from fabric.operations import local, run, put, env
+from os import path
+from datetime import datetime
+
+
+env.hosts = ['35.237.202.116', '34.73.195.113']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """ Deploy archive """
+
+    if not path.exists(archive_path):
+        return False
+
+    # send archive
+    put(archive_path, "/tmp/")
+
+    # save names
+    arch = archive_path.split('/')[1].split('.')[0]
+    archDir = "/data/web_static/releases/" + arch
+    remote_path = "/tmp/{}".format(archive_path.split('/')[1])
+
+    # make dir
+    run("mkdir -p {}".format(archDir))
+
+    # uncompress the archive
+    # -x: Extract files from an archive
+    # -z: Uncompress whit gzip command
+    # -f: use archive file or device ARCHIVE
+    # -C: Change to directory.
+    run("tar -xzf {} -C {}/".format(remote_path, archDir))
+
+    # delete the archive
+    run("rm {}".format(remote_path))
+
+    # move files
+    run("mv {}/web_static/* {}".format(archDir, archDir))
+
+    run("rm -rf {}/web_static".format(archDir))
+
+    # softlink to new deploy
+    run("rm -rf /data/web_static/current")
+
+    run("ln -s {}/ /data/web_static/current".format(archDir))
+
+    return True
