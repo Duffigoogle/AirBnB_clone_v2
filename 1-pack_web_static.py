870661562745df89c13e6c811e182f2aace32dd9
duffigoogle <calebdduff@gmail.com>
2022-10-20 21:12:46 +0000

    Add project files and modify provious stylings

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,17 +3,15 @@
 contents of the web_static folder"""
 
 from fabric.api import local
-import datetime
+from datetime import datetime
 
 def do_pack():
     """ Function to compress files i.e A Fabric script that
     generates a .tgz archive from the contents of the web_static """
     local("mkdir -p versions")
-    date = datetime.datetime.now()
-    date_format = date.strftime("%Y%m%d%H%M%S")
-    result_archive = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                           .format(date_format))
-    if result_archive.succeeded:
-        return "versions/web_static_{}.tgz".format(date_format)
-    else:
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
         return None
+    return result
